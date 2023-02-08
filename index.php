50947a9e296ef7c87e94a4a9c231cd0e0275fdb8
Hannan <abdulhannan0107@gmail.com>
2023-02-08 19:50:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <title>contact form tutorial</title>
   </head>
   <body>
+    <main>
     <p>SEND E-MAIL</p>
     <form class="contact-form" action="contactform.php" method="post">
       <input type="text" name="name" placeholder="Full Name">
@@ -13,5 +14,6 @@
       <textarea name="message" placeholder="Message"></textarea>
       <button type="submit" name="submit">SEND MAIL</button>
     </form>
+    </main>
   </body>
 </html>
